a859180f7a0c604bf00e9e856608fae31b269233
josephBabal <jbabal301@gmail.com>
2023-04-04 07:15:44 -0700

    added title

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title> Wordle English and Español </title>
         {/* Montserrat */}
         <link rel="preconnect" href="https://fonts.googleapis.com"/> 
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/> 
